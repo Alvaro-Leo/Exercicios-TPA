76382b1115b21c999a59c08cf0592955fff3b18d
Alvaro <alvaroandradesantos2009@gmail.com>
2025-08-30 12:44:59 -0300

    2- Adicionando labels Form2

--- ExerciciosTPA/Form1.Designer.cs
+++ ExerciciosTPA/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.tsmiLivros = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmiarquibancada = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmisalario = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblCriadores = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,7 +54,7 @@
             this.tsmiarquibancada,
             this.tsmisalario});
             this.exercíciosToolStripMenuItem.Name = "exercíciosToolStripMenuItem";
-            this.exercíciosToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.exercíciosToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
             this.exercíciosToolStripMenuItem.Text = "Exercícios";
             // 
             // tsmiLivros
@@ -77,15 +78,25 @@
             this.tsmisalario.Text = "Salário";
             this.tsmisalario.Click += new System.EventHandler(this.tsmisalario_Click);
             // 
+            // lblCriadores
+            // 
+            this.lblCriadores.AutoSize = true;
+            this.lblCriadores.Location = new System.Drawing.Point(12, 428);
+            this.lblCriadores.Name = "lblCriadores";
+            this.lblCriadores.Size = new System.Drawing.Size(341, 13);
+            this.lblCriadores.TabIndex = 1;
+            this.lblCriadores.Text = "Projeto realizado por Alvaro Andrade Santos e Leonardo Souza Novais";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCriadores);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Menu";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -100,6 +111,7 @@
         private System.Windows.Forms.ToolStripMenuItem tsmiLivros;
         private System.Windows.Forms.ToolStripMenuItem tsmiarquibancada;
         private System.Windows.Forms.ToolStripMenuItem tsmisalario;
+        private System.Windows.Forms.Label lblCriadores;
     }
 }
 
--- ExerciciosTPA/Form2.Designer.cs
+++ ExerciciosTPA/Form2.Designer.cs
@@ -31,38 +31,65 @@
             this.txtlivros = new System.Windows.Forms.TextBox();
             this.txtpreco = new System.Windows.Forms.TextBox();
             this.btncalcular = new System.Windows.Forms.Button();
+            this.lblQuant = new System.Windows.Forms.Label();
+            this.lblpreco = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtlivros
             // 
-            this.txtlivros.Location = new System.Drawing.Point(45, 85);
+            this.txtlivros.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtlivros.Location = new System.Drawing.Point(389, 73);
             this.txtlivros.Name = "txtlivros";
-            this.txtlivros.Size = new System.Drawing.Size(100, 20);
+            this.txtlivros.Size = new System.Drawing.Size(188, 38);
             this.txtlivros.TabIndex = 0;
-            this.txtlivros.Text = ".";
             // 
             // txtpreco
             // 
-            this.txtpreco.Location = new System.Drawing.Point(45, 151);
+            this.txtpreco.Enabled = false;
+            this.txtpreco.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpreco.Location = new System.Drawing.Point(389, 151);
             this.txtpreco.Name = "txtpreco";
-            this.txtpreco.Size = new System.Drawing.Size(100, 20);
+            this.txtpreco.Size = new System.Drawing.Size(188, 38);
             this.txtpreco.TabIndex = 1;
             // 
             // btncalcular
             // 
-            this.btncalcular.Location = new System.Drawing.Point(240, 131);
+            this.btncalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.Location = new System.Drawing.Point(419, 262);
             this.btncalcular.Name = "btncalcular";
-            this.btncalcular.Size = new System.Drawing.Size(84, 40);
+            this.btncalcular.Size = new System.Drawing.Size(130, 41);
             this.btncalcular.TabIndex = 2;
-            this.btncalcular.Text = "button1";
+            this.btncalcular.Text = "Calcular";
             this.btncalcular.UseVisualStyleBackColor = true;
             this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // lblQuant
+            // 
+            this.lblQuant.AutoSize = true;
+            this.lblQuant.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuant.Location = new System.Drawing.Point(78, 76);
+            this.lblQuant.Name = "lblQuant";
+            this.lblQuant.Size = new System.Drawing.Size(270, 31);
+            this.lblQuant.TabIndex = 3;
+            this.lblQuant.Text = "Quantidade de livros:";
+            // 
+            // lblpreco
+            // 
+            this.lblpreco.AutoSize = true;
+            this.lblpreco.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpreco.Location = new System.Drawing.Point(78, 140);
+            this.lblpreco.Name = "lblpreco";
+            this.lblpreco.Size = new System.Drawing.Size(152, 31);
+            this.lblpreco.TabIndex = 4;
+            this.lblpreco.Text = "Preço total:";
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblpreco);
+            this.Controls.Add(this.lblQuant);
             this.Controls.Add(this.btncalcular);
             this.Controls.Add(this.txtpreco);
             this.Controls.Add(this.txtlivros);
@@ -78,5 +105,7 @@
         private System.Windows.Forms.TextBox txtlivros;
         private System.Windows.Forms.TextBox txtpreco;
         private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Label lblQuant;
+        private System.Windows.Forms.Label lblpreco;
     }
 }

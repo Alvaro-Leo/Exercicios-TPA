2b81471e9ce6021c6764e1835911d9b714ef7a60
Alvaro <alvaroandradesantos2009@gmail.com>
2025-08-30 12:50:54 -0300

    3- Adicionando labels e textos Form3

--- ExerciciosTPA/Form3.Designer.cs
+++ ExerciciosTPA/Form3.Designer.cs
@@ -31,37 +31,65 @@
             this.txtarquibancada = new System.Windows.Forms.TextBox();
             this.btncalcular = new System.Windows.Forms.Button();
             this.txtTOT = new System.Windows.Forms.TextBox();
+            this.lblArquibancada = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtarquibancada
             // 
-            this.txtarquibancada.Location = new System.Drawing.Point(44, 35);
+            this.txtarquibancada.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtarquibancada.Location = new System.Drawing.Point(458, 77);
             this.txtarquibancada.Name = "txtarquibancada";
-            this.txtarquibancada.Size = new System.Drawing.Size(100, 20);
+            this.txtarquibancada.Size = new System.Drawing.Size(153, 38);
             this.txtarquibancada.TabIndex = 0;
             // 
             // btncalcular
             // 
-            this.btncalcular.Location = new System.Drawing.Point(266, 55);
+            this.btncalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.Location = new System.Drawing.Point(462, 295);
             this.btncalcular.Name = "btncalcular";
-            this.btncalcular.Size = new System.Drawing.Size(75, 23);
+            this.btncalcular.Size = new System.Drawing.Size(145, 43);
             this.btncalcular.TabIndex = 1;
-            this.btncalcular.Text = "button1";
+            this.btncalcular.Text = "Calcular";
             this.btncalcular.UseVisualStyleBackColor = true;
             this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
             // 
             // txtTOT
             // 
-            this.txtTOT.Location = new System.Drawing.Point(44, 99);
+            this.txtTOT.Enabled = false;
+            this.txtTOT.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTOT.Location = new System.Drawing.Point(458, 164);
             this.txtTOT.Name = "txtTOT";
-            this.txtTOT.Size = new System.Drawing.Size(100, 20);
+            this.txtTOT.Size = new System.Drawing.Size(153, 38);
             this.txtTOT.TabIndex = 2;
+            // 
+            // lblArquibancada
+            // 
+            this.lblArquibancada.AutoSize = true;
+            this.lblArquibancada.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArquibancada.Location = new System.Drawing.Point(93, 80);
+            this.lblArquibancada.Name = "lblArquibancada";
+            this.lblArquibancada.Size = new System.Drawing.Size(292, 31);
+            this.lblArquibancada.TabIndex = 3;
+            this.lblArquibancada.Text = "Quantidade do público:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(93, 167);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(235, 31);
+            this.lblTotal.TabIndex = 4;
+            this.lblTotal.Text = "Arrecadação total:";
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblArquibancada);
             this.Controls.Add(this.txtTOT);
             this.Controls.Add(this.btncalcular);
             this.Controls.Add(this.txtarquibancada);
@@ -77,5 +105,7 @@
         private System.Windows.Forms.TextBox txtarquibancada;
         private System.Windows.Forms.Button btncalcular;
         private System.Windows.Forms.TextBox txtTOT;
+        private System.Windows.Forms.Label lblArquibancada;
+        private System.Windows.Forms.Label lblTotal;
     }
 }

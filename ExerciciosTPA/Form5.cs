89b6a04b390a927b34314839618d6505e73bc27f
Alvaro <alvaroandradesantos2009@gmail.com>
2025-08-30 13:24:12 -0300

    6- Adicionando código Form5

--- ExerciciosTPA/Form5.cs
+++ ExerciciosTPA/Form5.cs
@@ -16,5 +16,35 @@ namespace ExerciciosTPA
         {
             InitializeComponent();
         }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            int COD;
+            COD = Convert.ToInt32(txtCod.Text);
+
+            switch(COD)
+            {
+                case 100:
+                    txtNome.Text = "Cachorro Quente";
+                    txtPreco.Text = "R$25,00";
+                    break;
+                case 101:
+                    txtNome.Text = "Bauru";
+                    txtPreco.Text = "R$15,00";
+                    break;
+                case 102:
+                    txtNome.Text = "X-Burguer";
+                    txtPreco.Text = "R$35,00";
+                    break;
+                case 103:
+                    txtNome.Text = "Triplo";
+                    txtPreco.Text = "R$47,00";
+                    break;
+                default:
+                    txtNome.Text = "Não existe";
+                    txtPreco.Text = "----------";
+                    break;
+            }
+        }
     }
 }
